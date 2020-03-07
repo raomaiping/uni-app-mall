feaaefe973a36fe8cd9d51ad743006f5b03dc384
raomaiping <2582395486@qq.com>
2020-03-07 17:17:57 +0800

    uniRequest请求数据

--- pages/tabBar/home/home.vue
+++ pages/tabBar/home/home.vue
@@ -1,13 +1,34 @@
 <template>
 	<!-- 自定义顶部导航栏 -->
-	<pageHeader/>
+	<pageHeader />
 </template>
 
 <script>
 	import pageHeader from "./pageHeader.vue"
-	export default{
-		components:{
+	import interfaces from "../../../utils/interfaces.js"
+	export default {
+		data(){
+			return {
+				swiperList:[],//轮播图片
+				categoryList:[], //分类
+				promotion:[] //活动
+			}
+		},
+		components: {
 			pageHeader
+		},
+		onLoad(){
+			this.initData();
+		},
+		methods: {
+			initData() {
+				uni.request({
+					url: interfaces.getMallData, 
+					success: (res) => {
+						console.log(res);
+					}
+				});
+			}
 		}
 	}
 </script>
---
+++ utils/interfaces.js
@@ -0,0 +1,7 @@
+const domain = "http://uni-app-mall.raomaiping.xyz/";
+const interfaces = {
+	//获取首页数据
+	getMallData: domain + "api/profiles/mall_list"
+}
+
+module.exports = interfaces;
